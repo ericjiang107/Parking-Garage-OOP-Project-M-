00daab42385736c9c95b0b76f989d246ffa7d1f5
Ala Ho <alaandho@gmail.com>
2021-07-18 15:46:20 -0400

    Erics final changes

--- ParkingGarage.py
+++ ParkingGarage.py
@@ -26,62 +26,78 @@
 import random
 
 
-
 class ParkingGarage():
     def __init__(self, tickets, parkingSpaces, paidTickets):
         self.tickets = tickets
         self.parkingSpaces = parkingSpaces
         self.paidTickets = paidTickets
-
-
+ 
     def takeTickets(self):
-        park_space = input('Please enter parking space letter on ticket: ')
+        park_space = input('Please enter parking space: ')
         rand_ticket = random.choice(self.tickets)
-        if park_space in self.parkingSpaces and rand_ticket in self.tickets:
-            self.parkingSpaces.remove(park_space)
+        if park_space.upper() in self.parkingSpaces and rand_ticket in self.tickets:
+            self.parkingSpaces.remove(park_space.upper())
             self.tickets.remove(rand_ticket)
-            print(f'Your ticket number is {rand_ticket} and your parking space is: {park_space}')
-        else:
-            print(f'Error: parking space {park_space} is occupied. Please try again.')
+            print(f'Your ticket number is {rand_ticket} and your parking space is {park_space.upper()}')
+        else:             
+            print(f'Error, parking space {park_space.upper()} is occupied. Please try again')
         return (rand_ticket, park_space)
+        
+            
+# # ParkingGarage([1,2,3,4,5], ['A','B','C','D','E'], {})
+# checkspace.takeTicket(['A','B','C','D','E'])
 
 
-# ParkingGarage([1,2,3,4,5], ['a','b','c','d','e'], {})
-# checkspace.takeTicket(['a','b','c','d','e'])
-
-
-    def payForParking(self, rand_ticket):
-        cell = "pay"   # --> Testing for pay
+    def payForParking(self, park_space):
+        cell = "pay" # --> Testing for pay
         if cell.lower() == "pay":
-            self.paidTickets[rand_ticket] = 'paid'
-            print(f'Thank you, your ticket has been paid: {self.paidTickets}')
+            num = input("Please enter parking space given: ")
+            if num.upper() == park_space.upper():
+                self.paidTickets[park_space.upper()] = 'paid'
+                print(f'Thank you, your ticket has been paid: {self.paidTickets}')
+            else: 
+                print("Invalid.")
         else:
             print("Error: please enter valid response")
-
-
+            
     def leaveGarage(self, rand_ticket, park_space):
         if "paid" in self.paidTickets.values():
             print('You have 15 minutes to leave. Have a nice day!')
-            self.parkingSpaces.append(park_space)
+            self.parkingSpaces.append(park_space.upper())
             self.tickets.append(rand_ticket)
+        else:
+            print("Wee woo Wee woo Wee woo, the cops are coming!")
+            
+            
+    def availability(self):
+        if self.parkingSpaces == []:
+            print("Garage full")
+        else:
+            print(f'Available parking spaces: {self.parkingSpaces}')
 
-
-# res = ParkingGarage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i','j'], {})
-# x = res.takeTickets()
-# res.payForParking(x) # passing x from function takeTickets to other functions
+            
+# res=ParkingGarage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ["A", 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', J'], {})      
+# x, y = res.takeTickets()
+# res.payForParking(y) # passing x from function takeTickets to other functions 
 # res.leaveGarage(x, y)
 
+
+# Assuming this is a machine
 def runGarage():
-    res = ParkingGarage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i','j'], {})
-    x = 0        # 0 is a placeholder --> x needs to be declared
-    y = ''       # empty string is a placeholder --> y needs to be declared
+    res = ParkingGarage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ["A", 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'], {})
+    x = 0 # 0 is a placeholder --> x needs to be declared
+    y = '' # empty string is a placeholder --> y needs to be declared
+    
     while True:
-        cell = input("Please enter what you would like to do: 'park', 'pay', 'leave'. ")
+        cell = input("Please enter what you would like to do: 'park', 'show', 'pay', 'leave' ")
         if cell.lower() == "park":
             x, y = res.takeTickets()
+        elif cell.lower() == "show":
+            res.availability()
         elif cell.lower() == "pay":
-            res.payForParking(x)
+            res.payForParking(y)
         elif cell.lower() == "leave":
             res.leaveGarage(x, y)
             break
-runGarage()
+            
+runGarage() 
